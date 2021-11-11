6253607e6fe2b65087a14d87ab3d7f549eb29fa0
Nine <pattadhonmt@gmail.com>
2021-11-11 20:36:38 +0700

    fit

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:gundamcore/pages/first.dart';
 import 'package:gundamcore/pages/gundamdetails.dart';
-
 import 'package:gundamcore/pages/gundampage.dart';
-import 'package:gundamcore/pages/listgundam.dart';
-
-
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),routes: {
 
@@ -35,7 +21,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -53,7 +38,6 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
